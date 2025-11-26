417cfa01d4a32e9ff32d38628001c4cb0ec6a4a1
Mostafa Elaasr <196775809+amrMohamed-pureSoft@users.noreply.github.com>
2025-11-26 05:30:55 +0200

    edit

--- lib/features/registration/presentation/widgets/registration_actions.dart
+++ lib/features/registration/presentation/widgets/registration_actions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 // import 'package:motion_toast/resources/arrays.dart';
