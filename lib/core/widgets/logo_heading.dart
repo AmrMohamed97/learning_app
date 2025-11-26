eae9167b704c3c1a3c6b96fee1e1545d860cb999
Mostafa Elaasr <196775809+amrMohamed-pureSoft@users.noreply.github.com>
2025-11-26 15:17:50 +0200

    edit logo

--- lib/core/widgets/logo_heading.dart
+++ lib/core/widgets/logo_heading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:talamiz_arina/core/helper/assets_helper.dart';
 import 'package:talamiz_arina/core/widgets/custom_title.dart';
 
@@ -12,17 +11,16 @@ class LogoHeading extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FractionallySizedBox(
-          widthFactor: .7,
-          child: Image.asset(Assets.assetsImagesPngTalamizSplash),
-        )
+        Image.asset(
+              Assets.assetsImagesPngTalamizSplash,
+              width: 200,
+              height: 100,
+            )
             .animate()
             .fade(duration: 600.ms)
             .scale(delay: 100.ms, duration: 600.ms, curve: Curves.elasticOut),
         const SizedBox(height: 35),
-        CustomTitle(
-          text: text,
-        )
+        CustomTitle(text: text)
             .animate()
             .fade(delay: 300.ms, duration: 600.ms)
             .slideY(begin: 0.2, end: 0, delay: 300.ms, duration: 600.ms),
